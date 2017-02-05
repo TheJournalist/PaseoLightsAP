d334b5e4ae3e9ce6a7acbdc82c6023686c167d22
AkyoFire <alexandre.ychu@gmail.com>
2017-02-05 15:26:35 -0800

    Add files via upload

--- www/index.js
+++ www/index.js
@@ -1,9 +1,9 @@
 var socket = io.connect('http://paseolights.local:80');
 
-function sendColors(r, g, b){
-    sendCMD(r);
-    setTimeout(sendCMD(g), 15);
-    setTimeout(sendCMD(b), 30);
+function sendColors(){
+    sendCMD(getred);
+    setTimeout(sendCMD(getgreen), 15);
+    setTimeout(sendCMD(getblue), 30);
 }
 
 function sendCMD(cmd){
