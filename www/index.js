ff499b66188f89ca936ed6c21945e15e334159bf
TheJournalist <natanschieck@hotmail.com>
2017-02-05 17:07:09 -0200

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,11 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 var socket = io.connect('http://paseolights.local:80');
 
+function sendCMD(cmd){
+    socket.emit('command', cmd);
+}
+
 $(document).on("click", ".click-led", function() {
-    socket.emit('command', $(this).val());
+    sendCMD($(this).val());
 });
