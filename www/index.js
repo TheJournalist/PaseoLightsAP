46354ee85f05ebf2effc85f84b2005dabd151067
TheJournalist <natanschieck@hotmail.com>
2017-02-06 00:23:05 -0200

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,6 +1,7 @@
 var socket = io.connect('http://paseolights.local:80');
 
 function sendCMD(cmd){
+    alert("oi");
     socket.emit('command', cmd);
 }
 
@@ -9,7 +10,7 @@ $(document).on("click", ".click-led", function() {
     var green=$("#green").slider("value");
     var blue=$("#blue").slider("value");
     sendCMD($(this).val());
-    setTimeout(sendCMD(red), 100);
-    setTimeout(sendCMD(green), 200);
-    setTimeout(sendCMD(blue), 300);
+    setTimeout(sendCMD(red), 5000);
+    setTimeout(sendCMD(green), 10000);
+    setTimeout(sendCMD(blue), 12000);
 });
