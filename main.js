3f00ecf1ba89477401d6cdf3c5e983dac7ce8fae
TheJournalist <natanschieck@hotmail.com>
2017-02-05 23:43:52 -0200

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function sendCMD(data)
   D7.write((data & 0x128) >> 7);
   
   CLK.write(1);
-  setTimeout(fall,10); //call the indicated function after 1 second (1000 milliseconds)
+  setTimeout(fall,10);
 }
 
 function fall()
