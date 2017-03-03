54f94ba4f1960a1ab735df31ef441800171a521e
TheJournalist <natanschieck@hotmail.com>
2017-03-02 22:32:01 -0300

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,8 +1,5 @@
 var socket = io.connect('http://paseolights.local:80');
 
 $(document).on("click", ".click-led", function() {
-    var red=$("#red").slider("value");
-    var green=$("#green").slider("value");
-    var blue=$("#blue").slider("value");
-    socket.emit('command', $(this).val(), red, green, blue);
+    socket.emit('command', $(this).val());
 });
